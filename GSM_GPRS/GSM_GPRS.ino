d6c029fb426d07b7f151ff59baa7f1110e0cbef0
Eduardo Araujo <eduardo.araujo0@outlook.com>
2017-05-19 22:44:54 -0300

    ***

--- GSM_GPRS/GSM_GPRS.ino
+++ GSM_GPRS/GSM_GPRS.ino
@@ -2,9 +2,11 @@
 #include <SoftwareSerial.h>
 #include "sms.h"
 #include "call.h"
+#include "inetGSM.h"
 
 SMSGSM sms;
 CallGSM call;
+InetGSM inet;
 
 boolean started = false;
 char smsbuffer[160];
@@ -24,18 +26,21 @@ void setup() {
     Serial.println("\nstatus=NOK");
   }
 
-  enviaSMS();
-  fazLigacao();
+  // enviaSMS();
+  // fazLigacao();
 }
 
 void loop() {
   if (started) {
     digitalWrite(LED_BUILTIN, HIGH);
 
-    if (gsm.readSMS(smsbuffer, 160, n, 20)) {
+    /* if (gsm.readSMS(smsbuffer, 160, n, 20)) {
       Serial.println(n);
       Serial.println(smsbuffer);
-    }
+    } */
+
+    Serial.println("-------------------------------------------------------------------------------------");
+    acessaHttp();
 
     digitalWrite(LED_BUILTIN, LOW);
     delay(1000);
@@ -57,4 +62,26 @@ void fazLigacao() {
    
   Serial.println("Desligando...");
   call.HangUp();
+}
+
+void acessaHttp() {
+  int numdata;
+  char msg[50];
+  
+  if (inet.attachGPRS("tim.br", "tim", "tim")){
+    Serial.println(F("status=Conectado..."));
+
+    gsm.SimpleWriteln("AT+CIFSR");
+    delay(5000);
+    gsm.WhileSimpleRead();
+
+    if(inet.httpPOST("localhost", 80, "/teste/add.php", "MSG_Texto1=teste", msg, 50)) {
+      Serial.println(F("Dados enviados"));
+      delay(5000);
+    } else {
+      Serial.println(F("Dados não enviados"));
+    }
+  } else {
+    Serial.println(F("status=Nao conectado !!"));
+  }
 }
