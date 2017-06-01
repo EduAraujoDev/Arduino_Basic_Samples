a0330cc0b010699ff93a3d4b81d44020d907846a
Eduardo Araujo <eduardo.araujo0@outlook.com>
2017-05-31 21:04:46 -0300

    Alterado projeto para componnte gsm separando chamada/sms

--- GSM_GPRS_CALL_SMS/GSM_GPRS_CALL_SMS.ino
+++ GSM_GPRS_CALL_SMS/GSM_GPRS_CALL_SMS.ino
@@ -2,16 +2,14 @@
 #include <SoftwareSerial.h>
 #include "sms.h"
 #include "call.h"
-#include "inetGSM.h"
 
 SMSGSM sms;
 CallGSM call;
-InetGSM inet;
 
 boolean started = false;
 char smsbuffer[160];
 char n[20];
-char numTelefone[12] = "11*********";
+char numTelefone[12] = "11********";
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -26,18 +24,18 @@ void setup() {
     Serial.println("\nstatus=NOK");
   }
 
-  // enviaSMS();
-  // fazLigacao();
+  enviaSMS();
+  fazLigacao();
 }
 
 void loop() {
   if (started) {
     digitalWrite(LED_BUILTIN, HIGH);
 
-    /* if (gsm.readSMS(smsbuffer, 160, n, 20)) {
+    if (gsm.readSMS(smsbuffer, 160, n, 20)) {
       Serial.println(n);
       Serial.println(smsbuffer);
-    } */
+    }
 
     Serial.println("-------------------------------------------------------------------------------------");
     acessaHttp();
@@ -56,32 +54,10 @@ void enviaSMS() {
 }
 
 void fazLigacao() {
-  Serial.println("\nDiscando....");
+  Serial.println("Discando....");
   call.Call(numTelefone);
   delay(10000);
    
   Serial.println("Desligando...");
   call.HangUp();
-}
-
-void acessaHttp() {
-  int numdata;
-  char msg[50];
-  
-  if (inet.attachGPRS("tim.br", "tim", "tim")){
-    Serial.println(F("status=Conectado..."));
-
-    gsm.SimpleWriteln("AT+CIFSR");
-    delay(5000);
-    gsm.WhileSimpleRead();
-
-    if(inet.httpPOST("localhost", 80, "/teste/add.php", "MSG_Texto1=teste", msg, 50)) {
-      Serial.println(F("Dados enviados"));
-      delay(5000);
-    } else {
-      Serial.println(F("Dados não enviados"));
-    }
-  } else {
-    Serial.println(F("status=Nao conectado !!"));
-  }
 }
